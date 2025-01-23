7c4dd0495989715587d3e28de37a5067e8a5c496
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2025-01-23 22:47:23 +0530

    operator_precedence_06.py

---
+++ operator_precedence_06.py
@@ -0,0 +1,8 @@
+#The operator precedence is like
+# first we have ** that is exponential operation 
+# secondly we have either multiplication or divison
+# Finally we have either addition or substraction
+
+res = 12 + 3 * 4 ** 2
+print(res) 
+# output : 60 

ab5cb96f66201cb95a80ceed5be89faa7027f818
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2025-01-24 20:55:51 +0530

    if_else_program_07.py

---
+++ if_else_program_07.py
@@ -0,0 +1,12 @@
+price_of_house = 1000000.0
+has_good_credit = False
+
+if has_good_credit:
+    down_payment = 0.1 * 1000000.0
+    print("You have a good credit so you need to pay 10% as down payment that is ",down_payment)
+else:
+    down_payment = 0.2 * 1000000.0
+    print("Since you have not good credit you need to pay 20% as down payment that is ",down_payment)
+
+print("Thank you for using the application ")
+

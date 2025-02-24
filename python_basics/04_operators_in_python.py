7b08de1ebdddb73fc53f96534b1f8047ced138cc
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2025-02-24 21:05:57 +0530

    Day-01_operators_in_python

---
+++ python_basics/04_operators_in_python.py
@@ -0,0 +1,54 @@
+#Date: 24th Feb 2025
+#Author : Amrit Manash
+
+#Operators in Python
+#Basically operators are used with variables to do some basic operations
+
+#1.Arithmetic operators
+a=10
+b=20
+print(a+b) # + operator
+print(b-a) # - operator
+print(a*b) # multiplication operator
+
+a=2
+b=3
+print(a**b) #power operator
+print(b/a)  #Normal Divsion operator
+print(b//a) #floor division operator
+
+a=2
+b=6
+print(b%a) #remainder division operator or modulo division operator
+           #It gives result as remainder value
+
+#2.Comparision operator
+a=2
+b=3
+print(b>a)
+print(a>b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+
+#3.Logical Operator like and,or,not
+a=2
+b=3
+
+print((b>a) and (a>b))
+print((b>a) or (a>b))
+print(not (a>b))
+
+#4.Assignment operators
+a=2
+b=3
+a=b #here we have used = to assign the value of b to a
+print(a)
+
+c=4
+c+=1 #it is called complex assignment operator equivalent to c=c+1
+print(c)
+
+
+
+

d8ea0d4a2bd1a3f0b2f6049b2575fc8e50d7790d
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2025-02-24 20:30:10 +0530

    Delete operator_precedence_06.py

--- operator_precedence_06.py
+++
@@ -1,9 +0,0 @@
-#The operator precedence is like
-# parenthesis or brackets 
-# secondly we have ** that is exponential operation 
-# third  we have either multiplication or divison
-# Finally we have either addition or substraction
-
-res = 12 + 3 * 4 ** 2
-print(res) 
-# output : 60 

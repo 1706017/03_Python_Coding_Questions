2ed7cfd75e718980a33a9a56cd8d332527737ec5
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2025-02-24 20:48:23 +0530

    Day-01_basics_type_Casting_in_python_commit

---
+++ python_basics/03_type_casting_in_python.py
@@ -0,0 +1,18 @@
+#Date : 24th Feb 2025
+#Author : Amrit Manash
+
+#Here we will see about type casting in python
+#Type casting means to change the data type of variable to another type
+
+age = input("Enter your age")
+
+#Here the type of age variable by default is string
+print(type(age))
+
+#Now we will change the data type of age variable from string to int
+print(type(int(age)))
+
+#in a similar way we can change the data type of int to sting
+#just by using str() on top of the variable that we want to chnage the
+#Type
+

8b3675ae85cfd5ebf36ca0d7e8be2be07579bd7c
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2025-02-24 20:32:20 +0530

    Day_01_commit_done_for_variables_and_data_types

---
+++ python_basics/01_variables_and_data_types.py
@@ -0,0 +1,51 @@
+#Date: 24th Feb 2025
+#Author: Amrit Manash
+
+#Topic: Variables
+
+#Variables are just like containers that can hold some values
+
+name = "Amrit Manash"
+print(name)
+
+id=1
+print(id)
+print(type(id)) #to know the type of data the variable is holding
+
+#Topic2 : Datatypes in python
+#int,float,bool,string,list,tuple,dictionary,set etc
+
+#data_type is float here
+marks=99.99
+print(type(marks))
+print(marks)
+
+#data_type is boolean here
+is_good_boy = True
+print(is_good_boy)
+print(type(is_good_boy ))
+
+#data_type is list here
+my_lst = [1,"Amrit",89]
+print(my_lst)
+
+#data_type is tuple here
+my_tuple = (1,2,"Amrit")
+print(type(my_tuple))
+
+#data_type is dictionary here
+age = 0
+my_dictionary = {
+    id : 1,
+    name : "Amrit",
+     age : 27
+}
+print(type(my_dictionary))
+
+
+#data_type is set here
+my_set = {1,3,3,4,"Amrit"}
+print(my_set)
+print(type(my_set))
+
+
